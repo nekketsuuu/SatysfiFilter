1dcf7d013a01b3aea26bde739ebd39d1fc6e3340
nekketsuuu <nekketsuuu@users.noreply.github.com>
2018-10-17 03:04:34 +0900

    Misc: add tests

--- test/MiscSpec.hs
+++ test/MiscSpec.hs
@@ -2,7 +2,8 @@ module MiscSpec (spec) where
 
 import Control.Monad (liftM2)
 import Data.Char (toLower, toUpper)
-import Data.List (all)
+import Data.List (all, lookup)
+import Data.Maybe (isNothing)
 
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
@@ -11,17 +12,22 @@ import Misc
 
 spec :: Spec
 spec = do
+  spec_caselessElem
+  spec_deleteAll
+
+spec_caselessElem :: Spec
+spec_caselessElem = do
   describe "caselessElem" $ do
     it "passes small manual cases" $ do
       caselessElem "def" ["abc", "def", "ghi"] `shouldBe` True
       caselessElem "DEF" ["ABC", "DEF", "GHI"] `shouldBe` True
       caselessElem "dEf" ["ABc", "deF", "gHI"] `shouldBe` True
-    prop "is safe for ASCII characters" prop_caselessElem_ascii
+    prop "is correct for ASCII characters" prop_caselessElem_ascii
 
 prop_caselessElem_ascii :: ASCIIString -> Property
 prop_caselessElem_ascii (ASCIIString str) =
-  forAll (stringLowerEq str) $ \str' ->
-  forAll (listsContains str') $ \strs ->
+  forAll (stringLowerEq str) $ \ str' ->
+  forAll (listsContains str') $ \ strs ->
   caselessElem str strs
   where
     toLU = elements [toLower, toUpper]
@@ -36,3 +42,19 @@ all2 :: (a -> a -> Bool) -> [a] -> [a] -> Bool
 all2 _ [] [] = True
 all2 f (x:xs) (y:ys) = (f x y) && (all2 f xs ys)
 all2 _ _ _ = False
+
+spec_deleteAll :: Spec
+spec_deleteAll = do
+  describe "deleteAll" $ do
+    it "passes small manual cases" $ do
+      deleteAll "f" [("f", "x")] `shouldBe` []
+      deleteAll "f" [("g", "x")] `shouldBe` [("g", "x")]
+      deleteAll "f" [("x", "f")] `shouldBe` [("x", "f")]
+    prop "result does not contain a given key" prop_deleteAll_no_key
+
+prop_deleteAll_no_key :: Integer -> Property
+prop_deleteAll_no_key k =
+  forAll allKvs $ \ kvs ->
+  isNothing $ lookup k $ deleteAll k kvs
+  where
+    allKvs = arbitrary :: Gen [(Integer, Integer)]
